3fa6f141624af280e49fc2917f44eaa4c3f5a695
DmitriyGovosyants <Demiangvs@gmail.com>
2022-11-14 17:51:18 +0200

    fix: mobile-menu

--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -4,35 +4,38 @@ import { device } from "styles/mediaquery";
 
 
 export const NavList = styled.ul`
-${device.tablet} {
-  margin-top: 95px;
-}
+  ${device.tablet} {
+    margin-top: 95px;
+  }
 
-${device.desktop} {
+  ${device.desktop} {
     display: flex;
     margin-top: 5px;
   }
 `;
 export const NavItem = styled.li`
-text-align: center;
-:not(:last-child) {
-  margin-bottom: 40px;
-}
+  text-align: center;
 
-${device.desktop}  {
   :not(:last-child) {
-    margin-right: 80px;
+    margin-bottom: 40px;
+  }
+
+  ${device.desktop}  {
+    :not(:last-child) {
+      margin-right: 80px;
   }
 }
 `;
 
 export const NavLinkStyled = styled(NavLink)`
+  display: block;
+  width: 100%;
+
   font-size: 32px;
   line-height: 1.37;
   letter-spacing: 0.04em;
-  display: block;
-  width: 100%;
   color: ${p => p.theme.colors.dark};
+
   &.active {
     color: ${p => p.theme.colors.accent};
     text-decoration: underline;
