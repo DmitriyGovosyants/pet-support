fd3d583824922752616e5251aabc3278c6794445
DmitriyGovosyants <Demiangvs@gmail.com>
2022-11-19 16:06:34 +0200

    fix: home-page img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,18 +8,6 @@ import {
 } from './HomePage.styled';
 
 const HomePage = () => {
-  const scrollHeight = Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.body.clientHeight,
-    document.documentElement.clientHeight
-  );
-  let viewHeight = window.innerHeight;
-  let pageHeight = document.documentElement.clientHeight;
-  console.log(viewHeight, pageHeight, scrollHeight);
-
   return (
     <BackgroundWrapper>
       <HomePageContainer>
