9e467914aac7c9926f683842a665a21cea4b00da
DmitriyGovosyants <Demiangvs@gmail.com>
2022-11-21 23:44:28 +0200

    add deploy baseUrl

--- src/redux/authApi.js
+++ src/redux/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8080/api',
+    baseUrl: 'https://petly-node-rest-api.onrender.com/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
 
