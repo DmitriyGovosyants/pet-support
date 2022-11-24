6ec314467c4e71dfc9b0cc0ad2892693fb568f66
DmitriyGovosyants <Demiangvs@gmail.com>
2022-11-25 00:49:36 +0200

    feat: spinner button add

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,17 +1,30 @@
 import { Dna } from 'react-loader-spinner';
 import { SpinnerWrapper } from './Spinner.styled';
 
-export const Spinner = () => {
+export const Spinner = ({ button }) => {
   return (
-    <SpinnerWrapper>
-      <Dna
-        visible={true}
-        height="180"
-        width="180"
-        ariaLabel="dna-loading"
-        wrapperStyle={{}}
-        wrapperClass="dna-wrapper"
-      />
+    <SpinnerWrapper button={button}>
+      {button && (
+        <Dna
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+          color="white"
+        />
+      )}
+      {!button && (
+        <Dna
+          visible={true}
+          height="180"
+          width="180"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+        />
+      )}
     </SpinnerWrapper>
   );
 };
