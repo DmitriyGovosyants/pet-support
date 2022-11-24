f6028f3166e53e71387f33f1c0eea4dea757f82e
Dmitriy Govosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-11-25 00:53:50 +0200

    Update Spinner.jsx

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -4,27 +4,14 @@ import { SpinnerWrapper } from './Spinner.styled';
 export const Spinner = ({ button }) => {
   return (
     <SpinnerWrapper button={button}>
-      {button && (
-        <Dna
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper"
-          color="white"
-        />
-      )}
-      {!button && (
-        <Dna
-          visible={true}
-          height="180"
-          width="180"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper"
-        />
-      )}
+      <Dna
+        visible={true}
+        height={button ? '80' : '180'}
+        width={button ? '80' : '180'}
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      />
     </SpinnerWrapper>
   );
 };
