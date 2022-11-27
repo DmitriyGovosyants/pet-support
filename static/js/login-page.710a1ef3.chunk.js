daa2efc45c0bc6b26064e18e9d3dfb251f105ff7
DmitriyGovosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-11-27 21:10:13 +0000

    Deploying to gh-pages from @ DmitriyGovosyants/pet-support@2e60de46ad98b3006916b2e9b934d09d2e8a13f9 🚀

---
+++ static/js/login-page.710a1ef3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[88],{6664:function(e,t,r){r.r(t);var a=r(4930),c=r(3329);t.default=function(){return(0,c.jsx)(a.Yb,{children:(0,c.jsx)(a.U0,{})})}}}]);
+//# sourceMappingURL=login-page.710a1ef3.chunk.js.map
