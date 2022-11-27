b3bfcfa05daa7609ce0dbffd056dca7376b9ed01
DmitriyGovosyants <Demiangvs@gmail.com>
2022-11-27 15:45:58 +0200

    fix

--- src/components/ModalOurFriend/ModalOurFriend.jsx
+++ src/components/ModalOurFriend/ModalOurFriend.jsx
@@ -1,43 +1,25 @@
-import { useEffect, useRef } from 'react';
-
+import { useEffect } from 'react';
 
 export const ModalOurFriend = ({ children, toggleModal }) => {
-    const isMounted = useRef(false);
-
-    useEffect(() => {
-        const handleKeyDown = e => {
-            if (e.code === 'Escape') {
-              toggleModal();
-            }
-          };
-
-        isMounted.current = true;
-        window.addEventListener('keydown', handleKeyDown);
-        
-        return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-        };
-    },[toggleModal]);
-
-    
-
-    const handleBackdropClick = e => {
-        if (e.target === e.currentTarget) {
-            toggleModal();
-        }
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
     };
 
-  return (
-    <div 
-    onClick={handleBackdropClick}
-    mounted={isMounted.current}
-    >
-        {children}
-    </div>
-   
-  );
-}
+    window.addEventListener('keydown', handleKeyDown);
 
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggleModal]);
 
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
+  };
 
-
+  return <div onClick={handleBackdropClick}>{children}</div>;
+};
