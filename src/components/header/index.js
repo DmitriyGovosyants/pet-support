8def38c6d66e21f4e2e218d3739fc81a91174889
DmitriyGovosyants <Demiangvs@gmail.com>
2022-11-29 14:33:18 +0200

    fix/folders

--- src/components/header/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Header/Header';
-export * from './Logo/Logo';
-export * from './Nav/Nav';
-export * from './AuthNav/AuthNav';
-export * from './UserNav/UserNav';
--- src/components/ourFriend/index.js
+++
@@ -1,3 +0,0 @@
-export * from './OurFriendsList/OurFriendsList';
-export * from './OurFriendItem/OurFriendItem';
-export * from './ModalOurFriend/ModalOurFriend';
